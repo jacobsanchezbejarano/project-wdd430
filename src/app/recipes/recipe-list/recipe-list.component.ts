bca2b453ee39e12ee454fdfdd6dca2dabde24146
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-05-18 14:41:45 -0400

    Services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe('Test', 'Test', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw2SrURyOwKDHSTqexsFMJB0&ust=1682569739130000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC34u7axv4CFQAAAAAdAAAAABAE'),
-  ];
-  constructor () {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
+
+  constructor (private recipeService: RecipeService) {
 
   }
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
+
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,32 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+    private recipes: Recipe[] = [
+        new Recipe(
+            'Test', 
+            'Test', 
+            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw2SrURyOwKDHSTqexsFMJB0&ust=1682569739130000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC34u7axv4CFQAAAAAdAAAAABAE'
+            ,[
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+            ),
+      ];
+    
+    constructor(private slService: ShoppingListService) {
+
+    }
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
+    }
+}
