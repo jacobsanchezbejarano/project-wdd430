1fe6040f014ebfddef02d11c28b7d3fb8b9524af
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-05-27 23:27:05 -0400

    Routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -16,6 +16,15 @@ export class RecipeService {
                 new Ingredient('French Fries', 20)
             ]
             ),
+            new Recipe(
+                'Test', 
+                'Test', 
+                'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw2SrURyOwKDHSTqexsFMJB0&ust=1682569739130000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC34u7axv4CFQAAAAAdAAAAABAE'
+                ,[
+                    new Ingredient('Meat', 1),
+                    new Ingredient('French Fries', 20)
+                ]
+                ),
       ];
     
     constructor(private slService: ShoppingListService) {
@@ -24,6 +33,10 @@ export class RecipeService {
 
     getRecipes(){
         return this.recipes.slice();
+    }
+
+    getRecipe(index: number){
+        return this.recipes[index];
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]){
