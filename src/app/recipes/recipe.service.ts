9a435b3c8a737a0e3bbc06d2097ce5e799d1773a
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-06-15 00:42:42 -0400

    Implementing http requests

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,29 +8,36 @@ import { Subject } from "rxjs";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
     recipeSelected = new Subject<Recipe>();
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Test', 
-            'Test', 
-            'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw2SrURyOwKDHSTqexsFMJB0&ust=1682569739130000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC34u7axv4CFQAAAAAdAAAAABAE'
-            ,[
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]
-            ),
-            new Recipe(
-                'Test', 
-                'Test', 
-                'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw2SrURyOwKDHSTqexsFMJB0&ust=1682569739130000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC34u7axv4CFQAAAAAdAAAAABAE'
-                ,[
-                    new Ingredient('Meat', 1),
-                    new Ingredient('French Fries', 20)
-                ]
-                ),
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Test', 
+    //         'Test', 
+    //         'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw2SrURyOwKDHSTqexsFMJB0&ust=1682569739130000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC34u7axv4CFQAAAAAdAAAAABAE'
+    //         ,[
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]
+    //         ),
+    //         new Recipe(
+    //             'Test', 
+    //             'Test', 
+    //             'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Feasy-recipes&psig=AOvVaw2SrURyOwKDHSTqexsFMJB0&ust=1682569739130000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNC34u7axv4CFQAAAAAdAAAAABAE'
+    //             ,[
+    //                 new Ingredient('Meat', 1),
+    //                 new Ingredient('French Fries', 20)
+    //             ]
+    //             ),
+    //   ];
+
+    private recipes: Recipe[] = [];
     
     constructor(private slService: ShoppingListService) {
 
+    }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
     }
 
     getRecipes(){
